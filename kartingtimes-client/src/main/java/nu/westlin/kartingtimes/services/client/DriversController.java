a4e03141d3a23ed35450873bba0f931b5fabe73b
Peter Vestlin <peter.vestlin@lm.se>
2015-11-16 08:35:03 +0100

    Added HTML and stuff.

---
+++ kartingtimes-client/src/main/java/nu/westlin/kartingtimes/services/client/DriversController.java
@@ -0,0 +1,27 @@
+package nu.westlin.kartingtimes.services.client;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Controller
+public class DriversController {
+
+    protected DriverRepository driverRepository;
+
+    @Inject
+    public DriversController(DriverRepository driverRepository) {
+        this.driverRepository = driverRepository;
+    }
+
+    @RequestMapping("/drivers")
+    public String drivers(Model model) {
+        List<Driver> accounts = driverRepository.getDrivers();
+        model.addAttribute("drivers", accounts);
+        return "drivers";
+    }
+
+}
