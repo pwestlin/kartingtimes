be0776565404a1e045b62f0ebae66ce2f56bdc39
Peter Vestlin <peter.vestlin@lm.se>
2015-11-27 21:33:59 +0100

    Fix and trix.

--- kartingtimes-webserver/src/main/java/nu/westlin/kartingtimes/services/web/HomeController.java
+++ kartingtimes-webserver/src/main/java/nu/westlin/kartingtimes/services/web/HomeController.java
@@ -1,7 +1,13 @@
 package nu.westlin.kartingtimes.services.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Home page controller.
@@ -11,9 +17,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/")
     public String home() {
         return "index";
     }
 
+    @RequestMapping(value = "/accounts/search", method = RequestMethod.GET)
+    public String laptimesSearchForm(Model model) {
+        model.addAttribute("searchCriteria", new SearchCriteria());
+        return "laptimesSearch";
+    }
+
+    @RequestMapping(value = "/laptimes/dosearch")
+    public String doSearch(Model model, SearchCriteria criteria,
+                           BindingResult result) {
+        logger.info("web-service search() invoked: " + criteria);
+
+        criteria.validate(result);
+
+        if (result.hasErrors())
+            return "laptimesSearch";
+
+        String accountNumber = criteria.getAccountNumber();
+        if (StringUtils.hasText(accountNumber)) {
+            return byNumber(model, accountNumber);
+        } else {
+            String searchText = criteria.getSearchText();
+            return ownerSearch(model, searchText);
+        }
+    }
 }
